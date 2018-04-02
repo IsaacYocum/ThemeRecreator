666430cfbb2830aa5a6fb14045687ecc59374185
Isaac Yocum <isyocum@gmail.com>
2018-04-01 19:26:34 -0600

    Initial Commit

---
+++ ThemeCreationPractice/Controllers/ThemeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThemeCreationPractice.Controllers
+{
+    public class ThemeController : Controller
+    {
+        // GET: Theme
+        public ActionResult Theme1()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ ThemeCreationPractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThemeCreationPractice.Startup))]
+namespace ThemeCreationPractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
